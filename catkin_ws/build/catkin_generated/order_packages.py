676d53ec6785ffd6e3135441bc6b30312935ce6b
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-02-17 12:57:19 +0900

    add publisher

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yamaguchi/test_ros/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yamaguchi/test_ros/catkin_ws/devel;/home/yamaguchi/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/yamaguchi/test_ros/catkin_ws/devel;/home/yamaguchi/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/test_pkg/CMakeFiles/test_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_pub.dir/src/test_pub.cpp.o"
+  "/home/yamaguchi/test_ros/catkin_ws/devel/lib/test_pkg/test_pub.pdb"
+  "/home/yamaguchi/test_ros/catkin_ws/devel/lib/test_pkg/test_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
